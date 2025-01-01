221a2fc8ad3c9c22cb56ad095f0fe7533c0c28e9
Sorina <lupusorina@yahoo.com>
2025-01-01 10:53:53 -0800

    cleanup and code organization

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# There is grass everywhere and there are sand pits.
-# The agent has to reach the goal position.
-# The agent has to avoid the sand pits.
+# There is grass everywhere and there are sand pits. The agent has to reach the goal position.
 
 import numpy as np
 import matplotlib
@@ -17,19 +15,17 @@ import yaml
 import sys
 
 # Custom imports.
-from systems import TwoDimSingleIntegratorDamping, TwoDimSingleIntegratorNominal
-from systems import TwoDimDoubleIntegratorDamping, TwoDimDoubleIntegratorNominal
+from systems import TwoDimDoubleIntegratorTrue, TwoDimDoubleIntegratorNominal
 from cem import CEM
 from plotting_utils import *
 from dino_nn import Dino
 
 # Set plotting style.
-font = {'family' : 'serif',
-        'serif' : 'Computer Modern Roman',
-        'size'   : 16}
-matplotlib.rc('font', **font)
-matplotlib.rcParams['text.usetex'] = True
-
+# font = {'family' : 'serif',
+#         'serif' : 'Computer Modern Roman',
+#         'size'   : 16}
+# matplotlib.rc('font', **font)
+# matplotlib.rcParams['text.usetex'] = True
 
 # World dim, divisible by the DINO patch size.
 w = 240
@@ -44,16 +40,10 @@ IDX_UX = 0
 IDX_UY = 1
 system = 'double_integrator'
 
-if system == 'single_integrator':
-    # State.
-    IDX_PX = 0
-    IDX_PY = 1
-
-if system == 'double_integrator':
-    IDX_PX = 0
-    IDX_VX = 1
-    IDX_PY = 2
-    IDX_VY = 3
+IDX_PX = 0
+IDX_VX = 1
+IDX_PY = 2
+IDX_VY = 3
 
 grass_img_path = 'images/grass.jpg'
 sand_img_path = 'images/sand.jpg'
@@ -87,7 +77,7 @@ def update_feat_rgb_and_damping_maps(xy, feature_map, rgb_map, damping_map, damp
     """
     for xcoord in x_coord_list:
         for ycoord in y_coord_list:
-            c = np.linalg.norm(xy - [xcoord, ycoord], axis=-1) < 7
+            c = np.linalg.norm(xy - [xcoord, ycoord], axis=-1) < 10
             counter_j_dino = 0
             counter_i_dino = 0
             for i in range(h):
@@ -131,12 +121,13 @@ if __name__ == "__main__":
     parser.add_argument("--model_folder", type=str, default="models/")
     parser.add_argument("--generate_dataset", type=bool, default=False)
     parser.add_argument("--use_NN", type=bool, default=False)
-    parser.add_argument("--run_cem", type=bool, default=False)
-    parser.add_argument("--horizon_planning", type=int, default=10)
 
-    parser.add_argument("--cem_num_particles", type=int, default=30)
-    parser.add_argument("--cem_num_iterations", type=int, default=100)
+    parser.add_argument("--horizon_planning", type=int, default=500)
+    parser.add_argument("--run_cem", type=bool, default=False)
+    parser.add_argument("--cem_num_particles", type=int, default=100)
+    parser.add_argument("--cem_num_iterations", type=int, default=1000)
     parser.add_argument("--cem_num_elite", type=int, default=10)
+    parser.add_argument("--cem_evaluate", type=bool, default=True)
     parser.add_argument("--cem_plot", type=bool, default=True)
     
     args = parser.parse_args()
@@ -187,26 +178,19 @@ if __name__ == "__main__":
 
     # 2D Damping Map.
     damping_map = np.ones((h, w))
-    if system == 'single_integrator':
-        damping_scaling = 0.01
-    if system == 'double_integrator':
-        damping_scaling = 5.0
+    damping_scaling = 5.0
 
     # Sand pits.
     update_feat_rgb_and_damping_maps(xy, feature_map, rgb_map, damping_map,
                                             damping_scaling=damping_scaling,
-                                            x_coord_list=[40, 90, 150, 210],
-                                            y_coord_list=[h//4, h//4, h//1.5, h//1.5],
+                                            x_coord_list=[w//2],
+                                            y_coord_list=[h//2],
                                             img_dino_map=sand_img_features_dino,
                                             img_rgb_map_lower_res=sand_img_lower_res)
 
     # Systems.
-    if system == 'single_integrator':
-        true_system = TwoDimSingleIntegratorDamping()
-        nominal_system = TwoDimSingleIntegratorNominal()
-    if system == 'double_integrator':
-        true_system = TwoDimDoubleIntegratorDamping()
-        nominal_system = TwoDimDoubleIntegratorNominal()
+    true_system = TwoDimDoubleIntegratorTrue()
+    nominal_system = TwoDimDoubleIntegratorNominal()
 
     # Generate Dataset for Training and Testing.
     if args.generate_dataset == True:
@@ -249,7 +233,7 @@ if __name__ == "__main__":
                     xs[t] = x
                     us[t] = np.array([u_x, u_y])
                     dino_features[t] = feature_map[int(x[IDX_PY] + 0.5), int(x[IDX_PX] + 0.5), :]
-                    x, _, _ = system.dynamics(x, us[t], dt, damping_map)
+                    x = system.dynamics(x, us[t], dt, damping_map)
 
                     # Turn around at edges of map.
                     mapshape = np.array(feature_map.shape)[[1, 0]]
@@ -269,7 +253,7 @@ if __name__ == "__main__":
                     ax.set(xlabel="x [m]", ylabel="y [m]", aspect='equal') #, title='Trajectory for ' + data_type)
 
             if gen_plots == True:
-                plt.savefig(args.plots_folder + str(i) + data_type + "_trajectory.pdf", dpi=200, transparent=True)
+                plt.savefig(args.plots_folder + str(i) + data_type + "_trajectory.pdf")
                 plt.show()
 
             return xs_total, us_total, dino_features_total, time_total_seconds
@@ -332,25 +316,20 @@ if __name__ == "__main__":
 
     # ---- Initial and Final Conditions ---
     # Set x0 and xT.
-    if system == 'single_integrator':
-        x0 = np.array([10.0, 25.0])
-        xT = np.array([x0[IDX_PX] + 60.0, x0[IDX_PY]])
-    if system == 'double_integrator':
-        x0 = np.array([10.0, 0.0, 25.0, 0.0])
-        xT = np.array([x0[IDX_PX] + 200.0, 0.0, x0[IDX_PY] + 40, 0.0])
+    x0 = np.array([w//2 - 40, 0.0, h//2, 0.0])
+    xT = np.array([w//2 + 40, 0.0, h//2, 0.0])
 
     fig, ax = plt.subplots(1, 2, figsize=(12, 6))
     ax[0].imshow(rgb_map)
-    ax[1].imshow(damping_map, cmap='jet')
-    plt.savefig(args.plots_folder + 'map.pdf', dpi=200)
-
-    fig, ax = plt.subplots()
-    ax.imshow(rgb_map, alpha=0.5)
-    ax.plot(x0[IDX_PX], x0[IDX_PY], "o", label="start")
-    ax.plot(xT[IDX_PX], xT[IDX_PY], "x", label="end")
-    ax.legend()
-    ax.set(xlabel="x [m]", ylabel="y [m]")
-    plt.show()
+    ax[0].plot(x0[IDX_PX], x0[IDX_PY], "o", label="start")
+    ax[0].plot(xT[IDX_PX], xT[IDX_PY], "x", label="end")
+    ax[0].legend()
+    cax1 = ax[1].imshow(damping_map, cmap='jet')
+    fig.colorbar(cax1, ax=ax[1], orientation='horizontal')
+    ax[0].set(xlabel="x [m]", ylabel="y [m]")
+    ax[1].set(xlabel="x [m]", ylabel="y [m]")
+    plt.tight_layout()
+    plt.savefig(os.path.join(args.plots_folder, "rgb_damping_map.png"))
 
     if args.run_cem == True:
         system_cem = nominal_system
@@ -366,22 +345,26 @@ if __name__ == "__main__":
                 cov=cov)
 
         if args.use_NN == True:
-            best_action, best_actions = cem.optimize(x0, system_cem, dt, xT, damping_map=damping_map,
+            best_action, best_actions, reward = cem.optimize(x0, system_cem, dt, xT, damping_map=damping_map,
                                                     terrain_map=feature_map, NN=[Phi_NN_mlp, A_trained])
         else:
-            best_action, best_actions = cem.optimize(x0, system_cem, dt, xT, damping_map=damping_map)
+            best_action, best_actions, reward = cem.optimize(x0, system_cem, dt, xT, damping_map=damping_map)
 
         t = np.arange(args.horizon_planning) * dt
+        print('Reward = ', reward)
 
+    if args.cem_evaluate == True:
+        best_action = np.load('actions/' + 'best_action' + str(args.cem_num_iterations - 1) + '.npy')
+        best_actions = np.zeros((args.cem_num_iterations, args.horizon_planning, 2))
+        for i in range(args.cem_num_iterations):
+            best_actions[i, :, :] = np.load('actions/' + 'best_action' + str(i) + '.npy')
+
+    if args.cem_plot:
         state = x0.copy()
         state_list = [state]
         state_list_all_actions = []
         for i in range(args.horizon_planning):
-            # if args.use_NN == True:
-            #     next_state, _, _ = true_system.dynamics(state, best_action[i], dt, feature_map, NN=[Phi_NN_mlp, A_trained])
-            # else:
-            next_state, _, _ = true_system.dynamics(state, best_action[i], dt, damping_map)
-
+            next_state = true_system.dynamics(state, best_action[i], dt, damping_map)
             state = next_state.copy()
             state_list.append(state)
 
@@ -389,34 +372,31 @@ if __name__ == "__main__":
             state = x0.copy()
             state_list_all_actions.append([state])
             for j in range(args.horizon_planning):
-                # if args.use_NN == True:
-                #     next_state, _, _ = true_system.dynamics(state, best_actions[i][j], dt, feature_map, NN=[Phi_NN_mlp, A_trained])
-                # else:
-                next_state, _, _ = true_system.dynamics(state, best_actions[i][j], dt, damping_map)
+                next_state = true_system.dynamics(state, best_actions[i][j], dt, damping_map)
                 state = next_state.copy()
                 state_list_all_actions[i].append(state)
 
         state_list = np.array(state_list)
 
-        if args.cem_plot:
-            # plot the trajectory
-            fig, ax = plt.subplots()
-            ax.imshow(rgb_map, alpha=0.5)
-            ax.set(xlabel="x [m]", ylabel="y [m]", aspect='equal')
-            for i in range(args.cem_num_iterations):
-                ax.plot(np.array(state_list_all_actions[i])[:, IDX_PX], np.array(state_list_all_actions[i])[:, IDX_PY], alpha=0.5)
-            ax.plot(state_list[:, IDX_PX], state_list[:, IDX_PY], label="true", linewidth=2, color='black')
-            ax.plot(x0[IDX_PX], x0[IDX_PY], "o", label="start")
-            ax.plot(xT[IDX_PX], xT[IDX_PY], "o", label="end")
-            ax.legend()
-            ax.set(xlabel="x", ylabel="y")
-
-            # plot us
-            fig, ax = plt.subplots()
-            ax.plot(best_action[:, IDX_UX], label="u_x", linewidth=2, color='black')
-            ax.plot(best_action[:, IDX_UY], label="u_y", linewidth=2, color='black')
-            plt.show()
-
         # save the best action
         np.save(args.dataset_folder + 'best_action_cem.npy', best_action)
         np.save(args.dataset_folder + 'best_state_cem.npy', state_list)
+
+        # plot the trajectory
+        fig, ax = plt.subplots()
+        ax.imshow(rgb_map, alpha=0.5)
+        ax.set(xlabel="x [m]", ylabel="y [m]", aspect='equal')
+        for i in range(args.cem_num_iterations):
+            ax.plot(np.array(state_list_all_actions[i])[:, IDX_PX], np.array(state_list_all_actions[i])[:, IDX_PY], alpha=0.5)
+        ax.plot(state_list[:, IDX_PX], state_list[:, IDX_PY], label="true", linewidth=2, color='black')
+        ax.plot(x0[IDX_PX], x0[IDX_PY], "o", label="start")
+        ax.plot(xT[IDX_PX], xT[IDX_PY], "o", label="end")
+        ax.set(xlabel="x", ylabel="y")
+        ax.legend()
+        plt.savefig(args.plots_folder + "cem_trajectory.png")
+
+        # plot us
+        fig, ax = plt.subplots()
+        ax.plot(best_action[:, IDX_UX], label="u_x", linewidth=2)
+        ax.plot(best_action[:, IDX_UY], label="u_y", linewidth=2)
+        plt.savefig(args.plots_folder + "best_action.png")
